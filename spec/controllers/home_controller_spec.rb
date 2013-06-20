451f48147ae8eb23b96f1d9fb697f76dc47d7e41
Colin Campbell <singfoom@gmail.com>
2013-06-20 14:05:48 -0500

    Basic spec for hooking up books to the home controller index

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -3,8 +3,16 @@ require 'spec_helper'
 describe HomeController do
 
   describe "GET 'index'" do
+
+    before do
+      @book = Fabricate(:book)
+      @book_two = Fabricate(:book)
+    end
+
     it "returns http success" do
       get 'index'
+      assigns(:books).should == Book.all
+      assigns(:books).length.should == 2
       response.should be_success
     end
   end
